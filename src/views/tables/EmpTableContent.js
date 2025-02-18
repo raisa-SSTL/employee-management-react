3aa8d55f3773211b97d05665c8c60a8f4c67d716
raisa-SSTL <raisa@synergysailtech.com>
2025-02-18 18:05:14 +0600

    added alert for emp delete button

--- src/views/tables/EmpTableContent.js
+++ src/views/tables/EmpTableContent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {
   Typography,
   Box,
@@ -8,17 +8,55 @@ import {
   TableHead,
   TableRow,
   Chip,
-  Button
+  Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, 
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useEmployees } from "../../context/EmployeeContext";
+import WarningAmberIcon from '@mui/icons-material/WarningAmber';
+import Swal from 'sweetalert2';
 
 const EmpTableContent = () => {
 
   const navigate = useNavigate();
   const { employees } = useEmployees(); //employee data fetched from context
+  const [open, setOpen] = useState(false);
+  const [selectedEmp, setSelectedEmp] = useState(null);
+
+  const handleDelete = (empId) => {
+    setSelectedEmp(empId);
+    setOpen(true);
+    // Swal.fire({
+    //   title: 'Are you sure?',
+    //   text: "This action cannot be undone.",
+    //   icon: 'warning',
+    //   showCancelButton: true,
+    //   confirmButtonColor: '#d33',
+    //   cancelButtonColor: '#3085d6',
+    //   confirmButtonText: 'Yes, delete it!',
+    //   cancelButtonText: 'Cancel',
+    // }).then((result) => {
+    //   // if (result.isConfirmed) {
+    //   //   // Perform the delete action here, for example:
+    //   //   console.log(`Employee with ID ${empId} has been deleted.`);
+    //   //   // You can add your delete logic here, like calling an API or updating state
+    //   // }
+    //   if (result.isConfirmed) {
+    //     Swal.fire({
+    //       title: "Deleted!",
+    //       text: "Your file has been deleted.",
+    //       icon: "success"
+    //     });
+    //   }
+    // });
+  };
+
+  const handleDialogClose = () => {
+    setOpen(false);
+    setSelectedEmp(null);
+  };
 
   return (
+    <>
     <Table
       aria-label="simple table"
       sx={{
@@ -160,7 +198,7 @@ const EmpTableContent = () => {
                         lg: 0,
                     },
                     }}
-                    // onClick={() => handleOpen()}
+                    onClick={() => handleDelete(employee.id)}
                 >
                     Delete
                 </Button>
@@ -169,6 +207,33 @@ const EmpTableContent = () => {
         ))}
       </TableBody>
     </Table>
+    {/* Confirm delete alert */}
+    <Dialog
+      open={open}
+      onClose={handleDialogClose}
+      aria-labelledby="delete-dialog-title"
+      aria-describedby="delete-dialog-description"
+    >
+    <DialogContent sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <WarningAmberIcon sx={{ color: 'orange', fontSize: '3rem', mb: 1 }} />
+        <DialogContentText 
+            id="delete-dialog-description" 
+            sx={{ textAlign: 'center' }}
+        >
+            Are you sure you want to delete this employee? 
+        </DialogContentText>
+    </DialogContent>
+    <DialogActions sx={{ justifyContent: 'center', mb:2 }}>
+        <Button onClick={handleDialogClose} color="primary">
+            Cancel
+        </Button>
+        <Button color="error">
+            Delete
+        </Button>
+    </DialogActions>
+  </Dialog>
+
+  </>
   );
 };
 
