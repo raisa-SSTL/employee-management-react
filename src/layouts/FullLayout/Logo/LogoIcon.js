f9133c3cc0cfdf23df39f00e45387d15ea5dbc6e
raisa-SSTL <raisa@synergysailtech.com>
2025-02-21 00:35:22 +0600

    added logo in navbar

--- src/layouts/FullLayout/Logo/LogoIcon.js
+++ src/layouts/FullLayout/Logo/LogoIcon.js
@@ -1,7 +1,10 @@
 import React from "react";
 import logoicn from "../../../assets/images/logo-dark.svg";
+import brotecslogo from "../../../assets/images/brotecslogo.png";
+
 const LogoIcon = (props) => {
-  return <img alt="Logo" src={logoicn} {...props} />;
+  // return <img alt="Logo" src={logoicn} {...props} />;
+  return <img alt="Logo" src={brotecslogo} width="230" height="50" {...props} />;
 };
 
 export default LogoIcon;
